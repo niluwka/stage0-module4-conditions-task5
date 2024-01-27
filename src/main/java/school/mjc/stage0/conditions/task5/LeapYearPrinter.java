086e8f5ce5c4d9f1c5c5dbebfc6533456a1802b6
niluwka0403 <ninimamatova0403@gmail.com>
2024-01-27 14:29:22 +0500

    Leap year printer.

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if (year == 365) {
+                System.out.println("leap");
+        } else if (year == 366) {
+            System.out.println("not leap");
+        }
     }
 }
